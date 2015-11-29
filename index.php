e4ef0e45fbf7add66b5241f13d010fc889f441e3
doucouredave <doucouredave@hotmail.com>
2015-11-29 16:28:33 +0100

    Case of table variable

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 
+//Using refernce of variable
 $a = "foobar";
 $b = &$a;
 $c = $b;
 $c = 1;
 
-echo "c = ".$c;
-echo "b = ".$b;
-echo "a = ".$a;
+echo "<h3>Using refernce of variable</h3><hr>";
+echo "c = ".$c."<hr>";
+echo "b = ".$b."<hr>";
+echo "a = ".$a."<hr>";
+
+
+// case of table variable
+$d = array("foo"=>"bar", 1 => 82);
+$e = $d["foo"];
+$f = &$d[1];
+$f = 'dvp';
+unset($d);
+
+echo "<h3>Case of table variable</h3><hr>";
+echo "f = ".$f."<hr>";
+echo "e = ".$e."<hr>";
